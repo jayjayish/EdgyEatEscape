c591f49017f92b22a1779120166bb737d2a71e74
brandonchan03 <brandonchan03@gmail.com>
2019-10-19 18:29:52 -0700

    Removed a lot of code. Actually works better. Guess I over thought it at first

--- Assets/Scripts/Camera_Movement.cs
+++ Assets/Scripts/Camera_Movement.cs
@@ -6,13 +6,6 @@ public class Camera_Movement : MonoBehaviour
 {
     GameObject player;
 
-    [Range(0, 1)]
-    [SerializeField]
-    public float speed;
-
-    private float offsets = 0.1f;
-    
-    // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -22,34 +15,9 @@ public class Camera_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (((int)gameObject.transform.position.x > (int)player.transform.position.x + offsets) ||
-            ((int)gameObject.transform.position.x < (int)player.transform.position.x - offsets) ||
-            ((int)gameObject.transform.position.y > (int)player.transform.position.y + offsets) ||
-            ((int)gameObject.transform.position.y < (int)player.transform.position.y - offsets)) {
+        if ((gameObject.transform.position != player.transform.position)) {
 
-            float x = player.transform.position.x;
-            float y = player.transform.position.y;
-            float hor = 0;
-            float ver = 0;
-
-            if (gameObject.transform.position.x < x)
-            {
-                hor = speed;
-            }
-            else if (gameObject.transform.position.x > x) {
-                hor = -speed;
-            }
-
-            if (gameObject.transform.position.y < y)
-            {
-                ver = speed;
-            }
-            else if (gameObject.transform.position.y > y)
-            {
-                ver = -speed;
-            }
-
-            gameObject.transform.position += new Vector3(hor, ver, 0);
+             gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + -10);
 
         }
     }
