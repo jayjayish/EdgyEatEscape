cecfa17d42b36d5648e7a2de816d1b9e8e01dbbd
Brandon Chan <brandonchan03@gmail.com>
2020-02-13 18:50:28 -0800

    Revert "Revert "Merge branch 'master' into Screen-Scroll""
    This reverts commit 6b4f7f7136465338516391f49a3995c42b318fac.

---
+++ Assets/Scripts/Character/HitboxPooler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxPooler : MonoBehaviour {
+    [SerializeField]
+    private List<Pool> pools;
+    private Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+    }
+
+   // #region Singleton
+    public static HitboxPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+  // #endregion Singleton
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            GameObject obj = Instantiate(pool.prefab);
+            obj.transform.parent = this.transform.parent;
+            obj.SetActive(false);
+            objectPool.Enqueue(obj);
+            poolDictionary.Add(pool.tag,  objectPool);
+        }
+	}
+
+    public GameObject SpawnFromPool(string tag, Vector3 position)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning(tag + " does not exist");
+            return null;
+        }
+       
+        GameObject newObj =   poolDictionary[tag].Dequeue();
+        newObj.SetActive(true);
+        newObj.transform.localPosition = position;
+       // newObj.transform.rotation = Quaternion.identity;
+
+        poolDictionary[tag].Enqueue(newObj);
+
+        return newObj;
+    }
+
+
+}
