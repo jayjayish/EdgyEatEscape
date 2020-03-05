de07466f8266dde16eafdd6039fc55a0d76d173c
Jayjay <jayjayish@gmail.com>
2020-03-04 18:25:55 -0800

    Modularized Movement

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : CharacterController
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
     private bool isAttacking = false;
+    private bool canMoveWhileAttacking = true;
     private float attackFrames = 0;
     private int playerLayer;
     private int enemyLayer;
@@ -284,88 +285,113 @@ public class PlayerController : CharacterController
        
 
 
-        if (isAttacking)
+        if (isAttacking && !canMoveWhileAttacking)
         {
+            attackMovementDelegate?.Invoke();
+        }
+        else if (isAttacking && canMoveWhileAttacking)
+        {
+
+            DetectBasicHorizontalMovement();
             attackMovementDelegate?.Invoke();
         }
         else
         {
 
-            move.x = Input.GetAxis("Horizontal");
+            DetectBasicHorizontalMovement();
+            DetectJump();
 
-            if (Input.GetButtonDown("Jump") && isGrounded) //checks if jump button is pressed while grounded
-            {
-                velocity.y = jumpTakeOffSpeed;
-            }
-
-            else if (Input.GetButtonUp("Jump")) // reduces velocity when user lets go of jump button
-            {
-                if (velocity.y > 0)
-                    velocity.y = velocity.y * 0.5f;
-            }
-
-            // detect dash
-
-            if (Input.GetButtonDown("DashLeft")) //checks if "a" or left arrow button was pressed
-            {
-
-                float timesinceLastLeft = Time.time - lastLeftTime;
-
-                if (timesinceLastLeft <= DOUBLE_PRESS_TIME)
-                {
-                    dashDirection = -1;
-                    dashTime = 0;//timer for dash
-                    Debug.Log("dashTime:" + dashTime);
-                    Debug.Log("double left");//delta tiime
-                }
-                //Double click
-                else
-                {
-                    //Normal Click
-                    lastLeftTime = Time.time;
-                }
-
-
-            }
-
-            if (Input.GetButtonDown("DashRight")) //checks if "d" or right arrow button was pressed
-            {
-
-                float timesinceLastRight = Time.time - lastRightTime;
-
-                if (timesinceLastRight <= DOUBLE_PRESS_TIME)
-                {
-                    dashDirection = 1;
-                    dashTime = 0;//timer for dash
-                    Debug.Log("dashTime:" + dashTime);
-
-                    Debug.Log("double right");//debug delta time
-                }
-                //double click
-                else
-                {
-                    //Normal Click
-                    lastRightTime = Time.time;
-                }
-            }
-
-            if (dashTime < startDashTime)
-            {
-                dashMultiplier = (1 - initialDashMultiplier) / startDashTime * dashTime + initialDashMultiplier;
-                move.x = dashDirection * dashMultiplier;
-                dashTime += Time.deltaTime; //Decrease time counter
-                IgnoreEnemyCollision(true);
-            }
-            else
-            {
-                IgnoreEnemyCollision(false);
-            }
+            DetectDash();
         }
 
 
         targetVelocity = move * maxSpeed;
 
     }
+
+    private void DetectBasicHorizontalMovement()
+    {
+        move.x = Input.GetAxis("Horizontal");
+    }
+
+    private void DetectJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded) //checks if jump button is pressed while grounded
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+
+        else if (Input.GetButtonUp("Jump")) // reduces velocity when user lets go of jump button
+        {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * 0.5f;
+        }
+    }
+
+    private void DetectDash()
+    {
+        // detect dash
+
+        if (Input.GetButtonDown("DashLeft")) //checks if "a" or left arrow button was pressed
+        {
+
+            float timesinceLastLeft = Time.time - lastLeftTime;
+
+            if (timesinceLastLeft <= DOUBLE_PRESS_TIME)
+            {
+                dashDirection = -1;
+                dashTime = 0;//timer for dash
+                Debug.Log("dashTime:" + dashTime);
+                Debug.Log("double left");//delta tiime
+            }
+            //Double click
+            else
+            {
+                //Normal Click
+                lastLeftTime = Time.time;
+            }
+
+
+        }
+
+        if (Input.GetButtonDown("DashRight")) //checks if "d" or right arrow button was pressed
+        {
+
+            float timesinceLastRight = Time.time - lastRightTime;
+
+            if (timesinceLastRight <= DOUBLE_PRESS_TIME)
+            {
+                dashDirection = 1;
+                dashTime = 0;//timer for dash
+                Debug.Log("dashTime:" + dashTime);
+
+                Debug.Log("double right");//debug delta time
+            }
+            //double click
+            else
+            {
+                //Normal Click
+                lastRightTime = Time.time;
+            }
+        }
+
+        if (dashTime < startDashTime)
+        {
+            dashMultiplier = (1 - initialDashMultiplier) / startDashTime * dashTime + initialDashMultiplier;
+            move.x = dashDirection * dashMultiplier;
+            dashTime += Time.deltaTime; //Decrease time counter
+            IgnoreEnemyCollision(true);
+        }
+        else
+        {
+            IgnoreEnemyCollision(false);
+        }
+    }
+
+
+
+
+
 
     private void IgnoreEnemyCollision(bool value)
     {
