beaaf05074271d2bf12760037de40a939f51ae7a
cheeshee <amyliu1.618@gmail.com>
2020-03-05 08:21:09 -0800

    add more finisher anims

---
+++ Assets/Scripts/Character/GeyserController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeyserController : MonoBehaviour
+{
+    // need timer for
+    [SerializeField] protected float timeToLive;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player gets control
+        // if timeout 
+    }
+}
