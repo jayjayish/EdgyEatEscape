8087771132f6770f397a2eb53dc4667fdd02e80c
Jayjay <jayjayish@gmail.com>
2020-03-25 22:42:09 -0700

    Make spawning not pain

---
+++ Assets/Scripts/EnemyMarkerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMarkerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string enemyType = "";
+    void Start()
+    {
+        GameObject enemy = ObjectPooler.Instance.SpawnFromPool(enemyType, transform.position + new Vector3(0f, 2f, 0f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
